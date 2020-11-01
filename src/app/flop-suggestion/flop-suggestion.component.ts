2cf059fa2d7c07878c11ed21230f9deaf07edccd
Daniel-bin <daniel.coughlan@outlook.ie>
2020-11-01 21:17:38 +0000

    Finished!

--- src/app/flop-suggestion/flop-suggestion.component.ts
+++ src/app/flop-suggestion/flop-suggestion.component.ts
@@ -4,19 +4,20 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
   templateUrl: "./flop-suggestion.component.html"
 })
 export class FlopSuggestionComponent {
-  suggestions = [
-    {
-      flopName: "",
-      year: "",
-      stars: ""
-    }
-  ];
+  suggestions = [];
 
   constructor() {}
 
   ngOnInit() {}
-  addSuggestion(suggestion) {
-    console.log(suggestion);
+  addSuggestion(fromVal) {
+    console.log(fromVal);
+    let suggestion = {
+      title: fromVal.flopName,
+      year: fromVal.year,
+      stars: fromVal.stars
+    };
+
     this.suggestions.push(suggestion);
+    console.log(this.suggestions[0]);
   }
 }
